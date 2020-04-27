6f827a552529b17f3cecd096baf1afb42963dcd2
David Garcia <davidgarcia1723@gmail.com>
2020-04-27 17:50:36 -0400

    Week-11-Challenges

---
+++ Week11Challenges/Easy-Challenges/MyFirstAsyncIO.js
@@ -0,0 +1,7 @@
+'use strict'
+const fs = require('fs')
+
+fs.readFile(process.argv[2], function (err, contents) {
+    var lines = contents.toString().split('\n').length - 1;
+    console.log(lines);
+});
